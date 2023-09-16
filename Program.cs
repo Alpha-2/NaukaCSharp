3b031b65e2d78a93c0b198e2200eac55c001e5eb
Alpha 2 <8784iirv5@gmail.com>
2023-09-16 14:08:24 +0200

    new commit

--- Program.cs
+++ Program.cs
@@ -6,10 +6,40 @@ using System.Threading.Tasks;
 
 namespace Nauka
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            double _userArgA;
+            double _userArgB;
+            try
+            {
+                Console.Write("enter the length of side A: ");
+                _userArgA = double.Parse(Console.ReadLine());
+                Console.Clear();
+                Console.Write("enter the length of side B: ");
+                _userArgB = double.Parse(Console.ReadLine());
+                Console.Clear();
+
+                Console.WriteLine(RectangleAreaCalc(_userArgA, _userArgB));
+                Console.WriteLine();
+                Console.WriteLine("-----To exit, press any key-----");
+
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static double RectangleAreaCalc(double sideA, double sideB)
+        {
+            double result = 0;
+
+            result = sideA * sideB;
+
+            return result;
         }
     }
 }
